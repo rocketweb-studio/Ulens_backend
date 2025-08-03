6b073310ad1e4368c5129e5fc0663db0e6416a8a
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-08-03 09:04:39 +0200

    feat: add deploy

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();

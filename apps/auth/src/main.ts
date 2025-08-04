3a540174b7082bbe210e5675eac99bb786bdc17f
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-08-04 10:46:57 +0200

    fix: fix prefix

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,7 +3,7 @@ import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api');
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();

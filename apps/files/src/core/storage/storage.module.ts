8588fdaf13ad98eefa513e0a2f31dca7fb8dc508
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-08-28 12:46:42 +0200

    feat: add storage

--- apps/files/src/core/storage/storage.module.ts
+++ apps/files/src/core/storage/storage.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 import { StorageConfig } from "@files/core/storage/storage.confg";
 import { StorageService } from "@files/core/storage/storage.service";
-import { StorageController } from "./storage.controller";
 
 @Global()
 @Module({
 	providers: [StorageService, StorageConfig],
-	controllers: [StorageController],
+	controllers: [],
 	exports: [StorageService],
 })
 export class StorageModule {}

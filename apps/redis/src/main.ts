2b3de198c33b6014f602fb749795ab703004ec42
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-09-07 15:05:45 +0200

    fix: deployment

--- apps/redis/src/main.ts
+++ apps/redis/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from "@nestjs/core";
-import { configApp } from "./redis.setup";
 import { RedisModule } from "./redis.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(RedisModule);
 
-	configApp(app);
 	await app.listen(process.env.REDIS_PORT as string);
 	console.log(`Redis is running on port ${process.env.REDIS_PORT}`);
 }

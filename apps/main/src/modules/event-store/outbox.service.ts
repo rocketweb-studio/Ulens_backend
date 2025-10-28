1b802de09649eea52600246474a4ec4b330aa052
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-10-28 15:35:57 +0100

    fix

--- apps/main/src/modules/event-store/outbox.service.ts
+++ apps/main/src/modules/event-store/outbox.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { IOutboxCommandRepository } from "@main/modules/event-store/outbox.interface";
-import { CreatePostDbOutputDto } from "@main/modules/post/dto/create-post-db.output.dto";
 
 @Injectable()
 export class OutboxService {
 	constructor(private readonly outboxCommandRepository: IOutboxCommandRepository) {}
 
-	async createOutboxPostEvent(dto: CreatePostDbOutputDto): Promise<string> {
-		return this.outboxCommandRepository.createOutboxPostEvent(dto);
-	}
+	// async createOutboxPostEvent(dto: CreatePostDbOutputDto): Promise<string> {
+	// 	return this.outboxCommandRepository.createOutboxPostEvent(dto);
+	// }
 
 	async findPendingOutboxEvents(chanckSize = 50): Promise<any[]> {
 		return this.outboxCommandRepository.findPendingOutboxEvents(chanckSize);

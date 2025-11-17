7dd896d06409f240087f5364ca65f6b68dc7d4d7
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-11-17 17:28:06 +0100

    fix deploy

--- apps/messenger/src/modules/messenger-rabbit/messenger-rabbit.module.ts
+++ apps/messenger/src/modules/messenger-rabbit/messenger-rabbit.module.ts
@@ -3,11 +3,10 @@ import { MessengerRabbitConsumer } from "@messenger/modules/messenger-rabbit/mes
 import { EventStoreModule } from "@messenger/modules/event-store/event-store.module";
 import { RMQ_EVENT_BUS } from "@libs/rabbit/rabbit.constants";
 import { RabbitEventBus } from "@libs/rabbit/rabbit.event-bus";
-import { MessengerModule } from "@messenger/modules/messenger/messenger.module";
 import { MessengerRabbitPublisher } from "@messenger/modules/messenger-rabbit/messenger-rabbit.publisher";
 
 @Module({
-	imports: [EventStoreModule, MessengerModule],
+	imports: [EventStoreModule],
 	providers: [MessengerRabbitConsumer, MessengerRabbitPublisher, { provide: RMQ_EVENT_BUS, useClass: RabbitEventBus }],
 	exports: [MessengerRabbitConsumer],
 })

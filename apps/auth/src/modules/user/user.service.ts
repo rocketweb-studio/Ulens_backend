3caa4b9ad231e4f1e9b46f75e85b09a74a837f8f
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-12-15 14:15:37 +0100

    fix

--- apps/auth/src/modules/user/user.service.ts
+++ apps/auth/src/modules/user/user.service.ts
@@ -237,7 +237,7 @@ export class UserService {
 		};
 
 		const refreshToken = await this.jwtService.signAsync(payloadForJwt, {
-			expiresIn: this.userEnvConfig.refreshTokenExpirationTime,
+			expiresIn: this.userEnvConfig.refreshTokenExpirationTime as any,
 			secret: this.userEnvConfig.refreshTokenSecret,
 		});
 
@@ -280,7 +280,7 @@ export class UserService {
 		await this.redisService.set(deviceId, JSON.stringify(payloadForJwt), "EX", 60 * 60 * 1000);
 
 		const refreshToken = await this.jwtService.signAsync(payloadForJwt, {
-			expiresIn: this.userEnvConfig.refreshTokenExpirationTime,
+			expiresIn: this.userEnvConfig.refreshTokenExpirationTime as any,
 			secret: this.userEnvConfig.refreshTokenSecret,
 		});
 
--- apps/gateway/src/microservices/auth/auth-client.service.ts
+++ apps/gateway/src/microservices/auth/auth-client.service.ts
@@ -104,8 +104,7 @@ export class AuthClientService implements IAuthClientService {
 	async login(loginDto: LoginDto, metadata: SessionMetadataDto): Promise<{ accessToken: string; refreshToken: string }> {
 		const { refreshToken, payloadForJwt } = await firstValueFrom(this.client.send({ cmd: AuthMessages.LOGIN }, { loginDto, metadata }));
 		const accessToken = await this.jwtService.signAsync(payloadForJwt, {
-			//@ts-expect-error
-			expiresIn: this.authEnvConfig.accessTokenExpirationTime,
+			expiresIn: this.authEnvConfig.accessTokenExpirationTime as any,
 			secret: this.authEnvConfig.accessTokenSecret,
 		});
 		const deviceId = payloadForJwt.deviceId;
@@ -125,8 +124,7 @@ export class AuthClientService implements IAuthClientService {
 		);
 
 		const accessToken = await this.jwtService.signAsync(payloadForJwt, {
-			//@ts-expect-error
-			expiresIn: this.authEnvConfig.accessTokenExpirationTime,
+			expiresIn: this.authEnvConfig.accessTokenExpirationTime as any,
 			secret: this.authEnvConfig.accessTokenSecret,
 		});
 
